824486b1ba0e361f3bd2203f89af0473b90e262d
horazio <hfusco@stuy.edu>
2018-12-13 18:21:07 -0500

    The program only alows one process to have the semophore at a time

--- sem.c
+++ sem.c
@@ -1,56 +1,69 @@
 #include "swag.h"
 
-union semun {
-    int val;
-    struct semid_ds * buf;
-    unsigned short * array;
-    struct seminfo * __buf;
-};
-
-
 int main(){
+  
     char inlin[3];
-    printf("What do you want?\n");
+    printf("What do you want?");
     fgets(inlin, 3, stdin);
+   
     //Open the file
-    int fd = open("swag.txt", O_CREAT | O_APPEND | O_WRONLY, 0777);
+    int fd = open(NAME, O_CREAT | O_APPEND | O_RDWR, 0777);
 
     //Check for the input
     if(!(strcmp(inlin, "-c"))){
 
         //SHARED MEMORY
-        int id;
-        char * data;
-
-        if((id = shmget(420, 200 * sizeof(char), 0664 | IPC_CREAT)) == -1){
+       
+       if(shmget(KEY, sizeof(int) , 0777 | IPC_CREAT) == -1){
             printf("Errror: %s\n", strerror(errno));
             exit(1);
         }
-        data = shmat(id, NULL, 0);
 
         //SEMOPHORES
-        int key = 0xD00D5EED;
-        semget(key, 1, IPC_CREAT);
+        int semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL);
+      
+        if(semd == -1){
+          printf("Errror: %s\n", strerror(errno));
+          exit(1);
+        }else{
+            union semun us;
+            us.val = 1;
+            semctl(semd, 0, SETVAL, us);
+        }
 
-        close(fd);
-    } else if(!(strcmp(inlin, "-r"))){
+        
+    } else {
+        //Reading all contents out of file
         struct stat info;
-
-
-        stat("swag.txt", &info);
+        stat(NAME, &info);
         int size = info.st_size;
-
         printf("%i", size);
-        //char * buffer = malloc(sizeof(char*) * size);
-
         char buffer[size];
         read(fd, buffer, size);
-
-        printf("--> %s <-- \n", buffer);
-
-    } else if(!(strcmp(inlin, "-v"))){
-
+       
+      //remaining choices
+      if(!(strcmp(inlin, "-r"))){
+        //Getting the semaphore
+        int semd = semget(KEY, 1, 0);
+        struct sembuf boof;
+        boof.sem_num = 0;
+        semop(semd, &boof, 1);
+        
+        //Shared memory gonzo
+        int id = shmget(KEY, sizeof(int), 0);
+        shmctl(id, IPC_RMID, NULL);
+        
+        //semophore gonzo
+        semctl(semd, 0, IPC_RMID);
+        
+        //Printing out the story
+        printf("Everything reset, this was the file:\n%s\n", buffer);
+      } else if(!(strcmp(inlin, "-v"))){
+        
+        printf("%s\n", buffer);
+      }
     }
 
+    close(fd);
     return 0;
 }
